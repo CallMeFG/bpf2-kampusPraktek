187e063589e4dfc58da5854e5dc1af83c1b7920e
Fathur Rizky Assani (CallMeFG) <assani200306@gmail.com>
2026-04-10 09:28:56 +0700

    first commit : perbaikan kecil

--- src/Pertemuan3/TailwindCSS.jsx
+++ src/Pertemuan3/TailwindCSS.jsx
@@ -2,7 +2,7 @@ import HitungGajiForm from "./HitungGajiForm"
 import UserForm from "./UserForm";
 export default function TailwindCSS() {
   return (
-    <div className="max-w-4xl mx-auto p-6 flex flex-col gap-6">
+    <div className="max-w-4xl mx-auto flex flex-col gap-6">
       <FlexboxGrid />
       <Typography />
       <p className="py-8 text-black">Belajar Tailwinds CSS</p>
