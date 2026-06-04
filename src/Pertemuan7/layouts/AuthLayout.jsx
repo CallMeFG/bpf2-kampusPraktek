9423ffd957ab8c97c88d5182cd170d411c882cb5
CallMeFG <assani200306@gmail.com>
2026-06-04 18:52:41 +0700

    Ubah text didalam file

--- src/Pertemuan7/layouts/AuthLayout.jsx
+++ src/Pertemuan7/layouts/AuthLayout.jsx
@@ -14,8 +14,7 @@ export default function AuthLayout() {
                 <Outlet/>
 
                 <p className="text-center text-sm text-gray-500 mt-6">
-                    © 2025 Sedap Restaurant Admin Dashboard. All rights
-                    reserved.
+                    © 2025 Sedap Restaurant Admin Dashboard.
                 </p>
             </div>
         </div>
